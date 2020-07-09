6e59c472e283711cfa8ee1a3f875f4df005cc3c0
edwin lopez <edwingl@uci.edu>
2020-07-08 21:16:28 -0700

    6.1 - 6.13

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,169 @@
+var budgetController = (function(){
+
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+
+
+    var data = {
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        totals:{
+            expo: 0,
+            inc: 0
+        } 
+    }
+
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+
+            //ID = Last ID + 1
+            //create new id
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length-1].id + 1; 
+            }else{
+                ID = 0;
+            }
+             
+            //create new item based on inc or exp type
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            }else if(type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            //push it into our data structure
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing: function(){
+            console.log(data);
+        }
+    };
+
+})();
+
+var UIController = (function(){
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
+    }
+
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMStrings.inputType).value, //will be inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: parseFloat(document.querySelector(DOMStrings.inputValue).value)
+            };
+        },
+        addListItem: function(obj, type){
+            var html, newHtml, element;
+            //create html string with placeholder text
+            if(type === 'inc'){
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }else if(type === 'exp'){
+                element = DOMStrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';       
+            } 
+
+            //replace the placeholder text with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //insert html into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+            
+        },
+        clearFields: function(){
+            var fields, fieldsArray;
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', '+ DOMStrings.inputValue); //returns a list
+
+            fieldsArray = Array.prototype.slice.call(fields);
+            
+            fieldsArray.forEach(function(current, index, array){
+                current.value = "";
+            });
+            //changes focus back to description input box
+            fieldsArray[0].focus();
+        },
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+            //which fold older browsers
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }   
+
+        });
+    }
+
+    var updateBudet = function(){   
+        //calculate the budget 
+        //return the budget
+        //display the budget on the UI
+    }
+
+    var ctrlAddItem = function(){
+        var input, newItem;
+
+        //get the input field data
+        input = UICtrl.getInput();
+
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+            //add the item to the budeget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            console.log(input.type);
+
+            //add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            //clear the fields
+            UICtrl.clearFields();
+
+            //calculate and update budget
+            updateBudet();
+        }
+
+    }
+
+    return{
+        init: function(){
+            console.log('Applciation has started.');
+            setupEventListeners();
+        }
+    }
+
+})(budgetController, UIController);
+
+
+controller.init();
