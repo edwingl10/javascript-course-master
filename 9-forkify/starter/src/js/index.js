801b9dec61ef9f0500ca2bfa4c56ead894945c2f
edwin lopez <edwingl@uci.edu>
2020-07-16 15:31:07 -0700

    9.24

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -96,3 +96,18 @@ const controlRecipe = async () =>{
 //window.addEventListener('load', controlRecipe);
 
 ['hasChange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+//handling recipe button clicks
+elements.recipe.addEventListener('click', e =>{
+    if(e.target.matches('.btn-decrease, .btn-decrease *')){
+        //decrease button clicked
+        if(state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    }
+    else if(e.target.matches('.btn-increase, .btn-increase *')){
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+});
