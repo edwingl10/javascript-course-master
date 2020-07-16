58971d40330b935da2c866ac4f32aff954209857
edwin lopez <edwingl@uci.edu>
2020-07-16 16:18:08 -0700

    9.26

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -2,6 +2,7 @@ import Search from './models/Search';
 import {elements, renderLoader, clearLoader} from './views/base';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import Recipe from './models/Recipe';
 import List from './models/List';
 
@@ -98,6 +99,31 @@ const controlRecipe = async () =>{
 
 ['hasChange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+const controlList = () =>{
+    //create new list if there is none yet
+    if(!state.list) state.list = new List();
+
+    //add each ingredient to the list
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+};
+
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    //handle the delete button
+    if(e.target.matches('.shopping__delete, .shopping__delete *')){
+        state.list.deleteItem(id);
+        listView.deleteItem(id);
+    }
+    else if(e.target.matches('.shopping__count-value')){
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
 //handling recipe button clicks
 elements.recipe.addEventListener('click', e =>{
     if(e.target.matches('.btn-decrease, .btn-decrease *')){
@@ -110,6 +136,9 @@ elements.recipe.addEventListener('click', e =>{
     else if(e.target.matches('.btn-increase, .btn-increase *')){
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } 
+    else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+        controlList();
     }
 });
 
